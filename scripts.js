8f1a9b13188fcb7d56b734a272a907ea7d4ad5a7
Pedro Augusto <pedroaugustooficial17@gmail.com>
2024-09-05 15:29:57 -0300

    tesla

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "28%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-100%"
+}
